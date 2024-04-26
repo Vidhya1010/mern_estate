fda3c72f0426068af25ca53fb22edf5570454f40
Vidhya1010 <vidhyabhale8@gmail.com>
2024-04-26 22:37:14 +0530

    update header search

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
+
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm,setSearchTerm]=useState('');
+const navigate = useNavigate();
+   const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+    useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+  
   return (
      <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -12,14 +33,15 @@ const Header = () => {
           </h1>
           </Link>
         <form
-          
+          onSubmit={handleSubmit}
           className='bg-slate-100 p-3 rounded-lg flex items-center'
         >
           <input
             type='text'
             placeholder='Search...'
             className='bg-transparent focus:outline-none w-24 sm:w-64'
-            
+            value={searchTerm}
+            onChange={(e)=> setSearchTerm(e.target.value)}
           />
           <button>
             <FaSearch className='text-slate-600' />
@@ -37,10 +59,17 @@ const Header = () => {
               About
             </li>
           </Link>
-          <Link to='/sign-in'>
-            
+          
+           <Link to='/profile'>
+            {currentUser ? (
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
               <li className=' text-slate-700 hover:underline'> Sign in</li>
-        
+            )}
           </Link>
         </ul>
           </div>
